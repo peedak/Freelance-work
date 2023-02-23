c9426560aecd6473f2f8ed43ca845aac2daf880d
Peeter Kraas <peeter.kraas@hotmail.com>
2023-02-23 14:29:02 +0200

    Github commit

---
+++ Exo_ms_trace.ps1
@@ -0,0 +1,95 @@
+#region 
+
+<# job description - 
+
+1. From a list of email addresses, together with an email subject OR messageid in a csv file
+2. iterate through the list of email addresses and the email subject.
+3. Perform a message trace for each email address and email subject using the Get-MessageTrace cmdlet
+4. Extract all the recipients
+5. Use a loop to iterate through all the recipients and perform a message trace on each recipient, together with an email subject that was identified in step 1
+6. Repeat steps 4 and 5 until there are no more results.
+7. Repeat steps 2 to 6 for all initial email addresses and subjects until there are no more results.
+8. the desired output will have all the email events and all its available fields to a csv file.
+9. create a log that logs the number of page and its message searched, Total number of message searched. and total time taken 
+
+Added no.9 the log file to follow https://cynicalsys.com/2019/09/13/working-with-large-exchange-messages-traces-in-powershell/
+#>
+
+#endregion
+
+# input the path of your .csv file here
+$list_input = "C:\temp\UPWORK\noja.csv"
+$list = Import-Csv $list_input -Delimiter ","
+
+# one way of measuring the time of the script running, I personally like this one the best
+$stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+
+# setting some primary variables
+$today = Get-Date
+$10_days = (Get-Date $today).AddDays(-10)
+$pageSize = 5000 # Max pagesize is 5000. There isn't really a reason to decrease this in this instance.
+$total_emails_searched = 0
+$total_pages_searched = 0
+
+# creating an array for the final output
+$final_output = @()
+
+# function for the message trace itself, takes two parameters - senderaddress and subject
+function message_trace {
+    param (
+        $senderaddress, $subject
+    )
+
+# paging setup
+$page = 1
+$message_list = @()
+
+do
+{
+    Write-Output "Getting page $page of messages..."
+    try {
+        $messagesThisPage = Get-MessageTrace -SenderAddress $senderaddress -StartDate $10_days -EndDate $today -PageSize $pageSize -Page $page
+    }
+    catch {
+        $PSItem
+    }
+    Write-Output "There were $($messagesThisPage.count) messages on page $page..."
+    $page++
+
+    # update the statistics variables
+    $script:total_emails_searched += $messagesThisPage.count
+    $script:total_pages_searched++
+
+    # filter the results based on the given email subject and add to our final output array
+    $script:final_output += ($messagesThisPage | Where-Object {$psitem.subject -like "*$subject*"})
+    $message_list += ($messagesThisPage | Where-Object {$psitem.subject -like "*$subject*"})    
+
+} until ($messagesThisPage.count -lt $pageSize)
+
+# call out the function itself again for each recipient
+foreach ($message_list_item in $message_list) {
+    message_trace -senderaddress $message_list_item.RecipientAddress -subject $message_list_item.subject
+}
+
+}
+
+# iterate through the given .CSV and run the message_trace function for each, included write-progress so you can see the progress
+$i = 1
+$list | ForEach-Object {
+    Write-Progress -Activity "Looping through the .csv" -status "$i of $($list.count)" -PercentComplete (($i / $list.count) * 100)
+    $i++
+    message_trace -senderaddress $psitem.senderaddress -subject $psitem.subject
+}
+
+Write-Progress -Activity "Looping through the .csv" -Status "Ready" -Completed
+
+# stop the stopwatch 
+$stopwatch.Stop()
+$total_time_taken = "$($stopwatch.Elapsed.Hours) Hours, $($stopwatch.Elapsed.Seconds) seconds"
+
+# export the final csv and logs
+$final_output | Export-Csv "C:\temp\UPWORK\Mitre_Auto\final_output.csv"
+
+$log_content = "Total number of pages searched - $total_pages_searched, total number of emails searched $total_emails_searched, total time taken $total_time_taken"
+$log_content | out-file "C:\temp\UPWORK\Mitre_Auto\log.txt"
+
